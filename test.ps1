c4ef9c966dfa28d01f95c8883efe79a9a648bc0b
Eric Wolf <ewolf@advitec.de>
2023-08-16 16:10:26 +0200

    source local setup before running tests
    This fixes the libraries not being found.

--- test.ps1
+++ test.ps1
@@ -1,1 +1,2 @@
+. .\install\local_setup.ps1
 colcon test --merge-install --packages-select ros2cs_tests; colcon test-result --verbose
--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+source ./install/local_setup.sh
 colcon test --merge-install --packages-select ros2cs_tests; colcon test-result --verbose
