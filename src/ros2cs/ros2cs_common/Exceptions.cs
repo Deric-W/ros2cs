2ddb47233326a171496c00093fdb9e7cc587b1d6
Eric Wolf <ewolf@advitec.de>
2023-08-16 13:58:36 +0200

    remove `DllLoadUtils`
    It has been replaced by `DllImport`.

--- src/ros2cs/ros2cs_common/Exceptions.cs
+++ src/ros2cs/ros2cs_common/Exceptions.cs
@@ -17,32 +17,6 @@ using System;
 
 namespace ROS2
 {
-    public class UnsatisfiedLinkError :Exception {
-      public UnsatisfiedLinkError () : base() { }
-      public UnsatisfiedLinkError (string message) : base (message) { }
-      public UnsatisfiedLinkError (string message, System.Exception inner) : base (message, inner) { }
-    }
-
-    public class UnknownPlatformError : Exception {
-      public UnknownPlatformError () : base() { }
-      public UnknownPlatformError (string message) : base (message) { }
-      public UnknownPlatformError (string message, System.Exception inner) : base (message, inner) { }
-    }
-
-    public class RuntimeError : Exception
-    {
-      public RuntimeError() : base() {}
-      public RuntimeError(string message) : base(message) {}
-      public RuntimeError(string message, Exception inner) : base(message, inner) {}
-    }
-
-    public class NotInitializedException : Exception
-    {
-      public NotInitializedException() : base() {}
-      public NotInitializedException(string message) : base(message) {}
-      public NotInitializedException(string message, Exception inner) : base(message, inner) {}
-    }
-
     public class InvalidNodeNameException : Exception
     {
       public InvalidNodeNameException() : base() {}
